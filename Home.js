0f63e96541e1a190e2d279eaa744f01c45d04b08
MichaelJaaa <113719137+MichaelJaaa@users.noreply.github.com>
2025-10-04 17:53:09 +0800

    Delete Home.js

--- Home.js
+++
@@ -1,6 +0,0 @@
-const menuToggle = document.getElementById('menu-toggle');
-const menuNav = document.getElementById('menu-nav');
-
-menuToggle.addEventListener('click', () => {
-    menuNav.classList.toggle('active');
-});
