e0988d504421c4edc8ba1e800a7ca4685f0ba8a9
MichaelJaaa <113719137+MichaelJaaa@users.noreply.github.com>
2025-10-04 17:53:19 +0800

    Add files via upload

---
+++ Home.js
@@ -0,0 +1,24 @@
+
+function openLinkedin() {
+    console.log("Test");
+    window.location = "linkedin://in/michael-jarod-rotoni-79271727a/";
+    setTimeout(() => {
+        window.location = "https://www.linkedin.com/in/michael-jarod-rotoni-79271727a/";
+    }, 1000);
+}
+
+function openInstagram() {
+    console.log("Test");
+    window.location = "instagram://user?username=_michaeljaaa";
+    setTimeout(() => {
+        window.location = "https://instagram.com/_michaeljaaa";
+    }, 1000);
+}
+
+function openFacebook() {
+    console.log("Test");
+    window.location = "fb://profile/jarod.rotoni";
+    setTimeout(() => {
+        window.location = "https://www.facebook.com/jarod.rotoni";
+    }, 1000);
+}
